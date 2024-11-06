2db39ce6dd73bde0fbc9a6dd2224e6d1bc2bbe1e
SowmiyaBalu <158642904+SowmiyaGuvi@users.noreply.github.com>
2024-11-06 21:16:44 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     <>
     <footer className="py-5 bg-dark">
         <div className="container">
-        <img src='/src/assets/image.png' className="img-fluid" alt="Guvi-blog-logo" />
+         <h3 style={{color:'white'}}>GUVI</h3>
         <br/>
         <br/>
         <p className="m-0 text-left text-white">GUVI – India’s Pioneering Vernacular EdTech, incubated by IIT-M, IIM-A, and a prestigious part of the <br/>HCL group. Empowering over 2.5 million global learners with top-tier educational solutions through a <br/>vernacular approach, we have revolutionized global tech education with strategic partnerships,<br/> including 'Google for Education' AICTE, AWS, Google Cloud, IIT-M Pravartak, UiPath, and NASSCOM.<br/> GUVI is your trusted guide to inclusive and accredited learning experiences.</p>
