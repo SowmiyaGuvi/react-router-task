9a261dbfa712117e5ec67ddc820aae6d611edc5e
SowmiyaBalu <158642904+SowmiyaGuvi@users.noreply.github.com>
2024-11-06 20:51:14 +0530

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -20,7 +20,7 @@ function Menu() {
           className={`navbar-brand ps-4 ${activeTab === '/' ? 'active' : ''}`}
           onClick={() => handleTabClick('/')}
         >
-          <img width="110" height="53" src= {assets.desiredNameOfAsset} className="img-fluid" alt="Guvi-logo" />
+          <img width="110" height="53" src= {assets} className="img-fluid" alt="Guvi-logo" />
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
